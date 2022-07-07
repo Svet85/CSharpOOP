4d3b6bbbcee929f4bcf2bdd536b9e07d71b8e764
svetlomirslavov <svetemilov@abv.bg>
2022-07-07 19:30:15 +0300

    exercises problems

---
+++ InterfacesAndAbstraction/Shapes/Circle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : IDrawable
+    {
+        public Circle(int radius)
+        {
+            Radius = radius;
+        }
+
+        public int Radius { get; set; }
+
+        public void Draw()
+        {
+            for (int i = -this.Radius; i <= this.Radius; i++)
+            {
+                for (int j = -this.Radius; j <= this.Radius; j++)
+                {
+                    int inside = (int)Math.Sqrt(Math.Pow(i, 2) + Math.Pow(j, 2));
+                    if (this.Radius == inside)
+                    {
+                        Console.Write('*');
+                    }
+                    else
+                    {
+                        Console.Write(' ');
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            //double rin = Radius - 0.4;
+            //double rout = Radius + 0.4;
+            //for (double i = Radius; i >= -Radius; --i)
+            //{
+            //for (double y = -Radius; y < rout; y += 0.5)
+            //{
+            //double value = i * i + y * y;
+
+            //if (value >= rin * rin && value <= rout * rout)
+            //{
+            //Console.Write("*");
+            //}
+            //else
+            //{
+            //Console.Write(" ");
+            //}
+            //}
+            //Console.WriteLine();
+            //}
+        }
+    }
+}
---
+++ InterfacesAndAbstraction/Shapes/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public void Draw()
+        {
+            Console.WriteLine(new string('*', this.Width));
+
+            for (int i = 0; i < this.Height - 2; i++)
+            {
+                Console.Write('*');
+                Console.Write(new string(' ', this.Width - 2));
+                Console.WriteLine('*');
+            }
+
+            Console.WriteLine(new string('*', this.Width));
+        }
+    }
+}
