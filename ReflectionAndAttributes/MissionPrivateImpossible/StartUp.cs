b1f94f38bda85597a3601fec4b93eedc31274ba0
svetlomirslavov <svetemilov@abv.bg>
2022-07-25 18:17:31 +0300

    exercises problems

---
+++ ReflectionAndAttributes/MissionPrivateImpossible/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace Stealer
+{
+    using System;
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string info = spy.RevealPrivateMethods("Stealer.Hacker");
+            Console.WriteLine(info);
+        }
+    }
+}
